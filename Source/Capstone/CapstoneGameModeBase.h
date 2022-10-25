caa070cc964a4adb4812375b55334e19dc334da0
sbprestwood <sbprestwood9991@gmail.com>
2022-10-25 16:05:07 -0500

    Initial commit

---
+++ Source/Capstone/CapstoneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CapstoneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONE_API ACapstoneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
